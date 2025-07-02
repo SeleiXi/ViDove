03ffc94c0a41c03e93f0868bb4c7456a55c3d2ef
SeleiXi <bzhanthresh@gmail.com>
2025-07-02 20:27:48 +0800

    feat: init bleu

--- evaluation/scores/bleu_eval.py
+++ evaluation/scores/bleu_eval.py
@@ -43,3 +43,11 @@ class BleuEvaluator:
             BLEU score
         """
         return self.bleu_model.sentence_score(mt, refs).score 
+    
+if __name__ == "__main__":
+    bleu_evaluator = BleuEvaluator()
+    # mt = "The player is encountering with the Blue Terran's SUV"
+    # mt = "The South Korea player is encountering with the Blue Terran's SCV"
+    mt = "asdasdasdas 123123 asdasda "
+    refs = ["The South Korea player is encountering with the Blue Terran's SCV"]
+    print(bleu_evaluator.evaluate_sentence(mt, refs))
